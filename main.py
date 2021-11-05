c318dfd20c928c47327edeca22914f2423ebe42e
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2021-11-05 15:50:15 +0100

    added more stuff

--- main.py
+++ main.py
@@ -8,4 +8,4 @@ bp = Blueprint("main", __name__)
 @bp.route("/")
 def index():
     movies = [ model.Movie(i, name, 'img/'+str(i)+'.jpg') for i, name, in zip(range(1,6), ['Black panter', 'After', 'Star Wars ', 'Justice league', 'It'])]
-    return render_template("main/home.html", movies=movies)
+    return render_template("main/home.html", movies=movies, all_movies=movies)
