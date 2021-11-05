89e1170f5c8cef0ddfcfec6ad7147e76e9a12106
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2021-11-05 19:57:56 +0100

    added the trailer_link atribute to the movie class

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 class Movie():
-    def __init__(self, id, name, image_path):
+    def __init__(self, id, name, image_path, trailer_link):
         self.id = id
         self.name = name
         self.image_path = image_path
+        self.trailer_link = trailer_link
