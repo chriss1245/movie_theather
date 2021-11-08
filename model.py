d77684eea272abb150440210f630ee0c5ac182cf
Jorge <jorpahermar@gmail.com>
2021-11-08 17:46:24 +0100

    adding description as a attribute of the class movie

--- model.py
+++ model.py
@@ -1,6 +1,7 @@
 class Movie():
-    def __init__(self, id, name, image_path, trailer_link = '0'):
+    def __init__(self, id, name, image_path, description = " ", trailer_link = '0'):
         self.id = id
         self.name = name
         self.image_path = image_path
+        self.description = description
         self.trailer_link = trailer_link
