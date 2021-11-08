4fb15f1341196af10de4b992d3bb7b75fabc17c0
Jorge <jorpahermar@gmail.com>
2021-11-08 17:53:46 +0100

    corrected typo in black panther's movie name

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ bp = Blueprint("main", __name__)
 
 @bp.route("/")
 def home():
-    movies = [ model.Movie(i, name, 'img/'+str(i)+'.jpg') for i, name, in zip(range(1,12), ['Black panter', 'After', 'Star Wars ', 'Justice league', 'Jaws', 'Joker', 'It', 'Titanic', 'Harry Potter', "Avatar", "Butterfly Effect"])]
+    movies = [ model.Movie(i, name, 'img/'+str(i)+'.jpg') for i, name, in zip(range(1,12), ['Black panther', 'After', 'Star Wars ', 'Justice league', 'Jaws', 'Joker', 'It', 'Titanic', 'Harry Potter', "Avatar", "Butterfly Effect"])]
     return render_template("main/home.html", movies=movies[5:], all_movies=movies)
 
 @bp.route("/reservation") # Requires the movie id
