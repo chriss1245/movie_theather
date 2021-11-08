f79b2554e9c396f5c674596036211cb0009fa7b1
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2021-11-08 16:37:22 +0100

    aded some extra files

--- main.py
+++ main.py
@@ -6,6 +6,14 @@ from . import model
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
-def index():
+def home():
     movies = [ model.Movie(i, name, 'img/'+str(i)+'.jpg') for i, name, in zip(range(1,12), ['Black panter', 'After', 'Star Wars ', 'Justice league', 'Jaws', 'Joker', 'It', 'Titanic', 'Harry Potter', "Avatar", "Butterfly Effect"])]
     return render_template("main/home.html", movies=movies[5:], all_movies=movies)
+
+@bp.route("/reservation") # Requires the movie id
+def reservation(movie_id = 1):
+    return render_template("main/reservation.html")
+
+@bp.route("/movie") # will take the movie id
+def movie(movie_id = 1):
+    return render_template("main/movie.html")
