cd3a71daca78e9638f4d4cf8eb4e7166cc751f9f
Marco Narganes <marconarganesf@gmail.com>
2021-11-15 17:40:46 +0100

    Login hecho(provisional)

--- main.py
+++ main.py
@@ -18,14 +18,11 @@ def reservation(movie_id = 1):
 def movie(movie_id = 1):
     return render_template("main/movie.html")
 
-<<<<<<< HEAD
 @bp.route("/login")
 def login():
     return render_template("main/login.html")
 
-=======
 @bp.route("/customer")
 def user_teemplate():
     user = model.User("lolito", 'lolito@lolito.com', "legoland")
     return render_template("main/user_template.html",user = user)
->>>>>>> e256d5611565f9e6094bfcea39b7c7d73f4fdb28
--- model.py
+++ model.py
@@ -11,41 +11,6 @@ class Movie():
 class Screen():
     def __init__(self, id):
         self.id = id
-<<<<<<< HEAD
-
-class MovieProjection():
-    def  __init__(self, id, screen_id, movie_id, date):
-        self.id = id
-        self.screen_id = screen_id
-        self.movie_id = movie_id
-        self.date = date
-
-class User():
-    def __init__(self, name, email, location):
-        self.name = name
-        self.email = email
-        self.location = location
-
-
-	movie=Movie(db.Model).name
-	screen=Screen(db.Model).name
-	day=db.Column(db.Integer, primary_key=True)
-	hour=db.Column(db.Integer, primary_key=True)
-	minute=db.Column(db.Integer, primary_key=True)
-
-
-
-	"""
-	def __init__(self, movie, screen, day, time):
-		self.movie=movie
-		self.screen=screen
-		self.day=day
-		self.time=time
-	"""
-
-class Reservation(db.Model ):
-=======
->>>>>>> e256d5611565f9e6094bfcea39b7c7d73f4fdb28
 
 class MovieProjection():
     def  __init__(self, id, screen_id, movie_id, date):
