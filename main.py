366b814a32a41a668adf83c3d41a4755bae71303
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2021-12-01 14:08:27 +0100

    connected user view and admin view

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ def home():
     movies = model.Movie.query.all()
     return render_template("main/home.html", movies=movies[:5], all_movies=movies)
 
+#-------------------RESERVATION---------------------------------
 @bp.route("/reservation/<int:movie_id>") # Requires the movie id
 def reservation(movie_id = 1):
     movie = model.Movie.query.filter_by(id=movie_id).first_or_404()
@@ -22,7 +23,12 @@ def post_reservation(movie_id= 0):
 
     # Get the projection
     projection_id = int(request.form.get("movie_projection_id"))
-    projection = model.Projection.query.filter_by(id=projection_id).first_or_404()
+    projection = (model.Projection.query
+        .filter_by(id=projection_id)
+        .filter(model.Projection.date >= datetime.datetime.now())
+        .first())
+    if not projection:
+        abort(403, "Movie projection not available, try another")
     
     # Get the remaining seats in the movie projection
     remaining_seats = projection.screen.seats
@@ -37,7 +43,7 @@ def post_reservation(movie_id= 0):
     # Proceed with the reservation
     reservation = model.Reservation(
         projection_id=projection.id,
-        user_id=1,
+        user_id=3,
         seats=seats,
         date=datetime.datetime.now()
     )
@@ -45,20 +51,57 @@ def post_reservation(movie_id= 0):
     db.session.commit()
     return redirect(url_for('main.user_template'))
 
+#-----------------------MOVIE----------------------------------
 @bp.route("/movie/<int:movie_id>") # will take the movie id
 def movie(movie_id=1):
     movie = model.Movie.query.filter_by(id=movie_id).first_or_404()
     return render_template("main/movie.html", movie=movie)
 
+#-----------------------USER--------------------------------------
+@bp.route("/user")
+def user_template():
+    user = model.User.query.filter_by(id=3).first_or_404()
+
+    # If the user is not admin it loads the customer view else the admin view
+    if not user.admin:
+
+        # Reservations
+        reservations_before = (model.Reservation.query
+            .filter_by(user_id=user.id)
+            .filter(model.Reservation.date < datetime.datetime.now())
+            .order_by(model.Reservation.date)
+            .all()
+        )
+
+        reservations_after = (model.Reservation.query
+            .filter_by(user_id=user.id)
+            .filter(model.Reservation.date >= datetime.datetime.now())
+            .order_by(model.Reservation.date)
+            .all()
+        )
+        return render_template("main/user_template.html",
+            user=user,
+            reservations_after=reservations_after,
+            reservations_before=reservations_before)
+    else:
+        movies = model.Movie.query.all()
+        screens = model.Screen.query.all()
+        return render_template("main/admin_template.html",
+            movies=movies,
+            screens=screens,
+            user=user)
+
+@bp.route("/user", methods=["POST"])
+def post_user():
+    # Logic of web site review and movie projection creation
+    return redirect(url_for("main.user"))
+
+#----------------------LOGIN--------------------------------------
 @bp.route("/login")
 def login():
     return render_template("main/login.html")
 
-@bp.route("/customer")
-def user_template():
-    return render_template("main/user_template.html",user = None, movies_after = [], movies_before = [])
-
-
+#-----------------------SIGNUP--------------------------------------
 @bp.route("/register")
 def register():
     return render_template("main/register.html")
