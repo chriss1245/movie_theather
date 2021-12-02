43760756f89b066d3978cc16da432af998c59fbd
Jorge <jorpahermar@gmail.com>
2021-12-02 22:28:41 +0100

    modified nuances to comply with PEP 8 371

--- analytics.py
+++ analytics.py
@@ -1,14 +1,11 @@
 from textblob import TextBlob
-
 import matplotlib.pyplot as plt
 import base64
 import io
 
-# añadir clase reviews?
-
 
 def web_analytics(reviews):
-    # input is a list of instances of class Review
+
     n = len(reviews)
     labels = 'Positive', 'Negative', 'Neutral'
     positives, negatives, neutrals = 0, 0, 0
@@ -23,10 +20,10 @@ def web_analytics(reviews):
     per_neg, per_neutral, per_positives = negatives/n, neutrals/n, positives/n
     plt.pie([per_neg, per_positives, per_neutral], labels=labels, autopct='%1.1f%%', shadow=True, startangle=90)
     plt.title("Site reviews")
-    pic_IObytes = io.BytesIO()
-    plt.savefig(pic_IObytes, format='png')
-    pic_IObytes.seek(0)
-    pic_hash = base64.b64encode(pic_IObytes.read())
+    pic_iobytes = io.BytesIO()
+    plt.savefig(pic_iobytes, format='png')
+    pic_iobytes.seek(0)
+    pic_hash = base64.b64encode(pic_iobytes.read())
     return pic_hash
 
 
@@ -34,11 +31,8 @@ def movie_analytics(movies):
     # generates a barplot and converts it to base 64
     plt.bar(*zip(*movies.items()))
     plt.title("Average movie reviews")
-    pic_IObytes = io.BytesIO()
-    plt.savefig(pic_IObytes, format='png')
-    pic_IObytes.seek(0)
-    pic_hash = base64.b64encode(pic_IObytes.read())
+    pic_iobytes = io.BytesIO()
+    plt.savefig(pic_iobytes, format='png')
+    pic_iobytes.seek(0)
+    pic_hash = base64.b64encode(pic_iobytes.read())
     return pic_hash
-
-
-
