ce841e7f1a63b7a854950f5101b705c08b473637
Christopher Manzano <christopher.manzano.vimos@gmail.com>
2021-12-13 11:52:37 +0100

    using emial for notifying changes in reservations

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from flask import Blueprint, render_template, request, url_for, \
     redirect, abort, flash
 from . import model, db, bcrypt, analytics, csrf
 import flask_login, datetime, imghdr, os
+from .utils import cancellation_emails, reservation_email
 
 bp = Blueprint("main", __name__)
 
 #-----------------------------HOME-------------------------------------------------
 @bp.route("/")
 def home():
-    flask_login.login_user(model.User.query.filter_by(id=3).first()) #TO REMOVE!!!!! AUTMOATIC LOGIN
     movies = model.Movie.query.order_by(model.Movie.rating.desc()).limit(5).all() # Gets the 5 best rated movies
     movies.append(movies[0]) # Needed for the slider animation 
     all_movies = model.Movie.query.order_by(model.Movie.id.desc()).all() # All movies by newest
@@ -80,6 +80,7 @@ def post_reservation(movie_id= 0):
         seats=seats,
         date=datetime.datetime.now()
     )
+    reservation_email(reservation)
     db.session.add(reservation)
     db.session.commit()
     return redirect(url_for('main.user_template'))
@@ -221,6 +222,8 @@ def post_user():
             db.session.commit()
         elif form_type == "cancel":
             projection_id = int(request.form.get("projection_id"))
+            projection = model.Projection.query.filter_by(id=projection_id).first_or_404()
+            cancellation_emails(projection.reservations)
             model.Projection.query.filter_by(id=projection_id).delete()
             db.session.commit()
 
