56ad78992cad0e93667dd4b1372531ac712a3d5b
Christopehr Manzano <christopher.manzano.vimos@gmail.com>
2021-12-13 12:47:01 +0100

    setting up server in production

--- __init__.py
+++ __init__.py
@@ -26,7 +26,7 @@ def create_app(test_config=None):
     # SQL ALCHEMY
     
     app.config["SQLALCHEMY_DATABASE_URI"] =\
-        "mysql+mysqldb://22_appweb_20:ch2663s7@mysql.lab.it.uc3m.es/22_appweb_20b"
+        "mysql+mysqldb://22_appweb_20:ch2663s7@mysql.lab.it.uc3m.es/22_appweb_20c"
     """
     app.config['SQLALCHEMY_DATABASE_URI']=\
         "mysql://chris:yolo@localhost/theather"
