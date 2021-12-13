ac764750510e53df106d5da1bb00299a2847b6bc
Marco Narganes <marconarganesf@gmail.com>
2021-12-13 17:19:24 +0100

    poca cosa

--- analytics.py
+++ analytics.py
@@ -50,34 +50,3 @@ def movie_analytics(movies):
     pic_hash = base64.b64encode(pic_IObytes.read())
     return pic_hash
 
-"""
-def quicksort(array):
-    # If the input array contains fewer than two elements,
-    # then return it as the result of the function
-    if len(array) < 2:
-        return array
-
-    low, same, high = [], [], []
-
-    # Select your `pivot` element randomly
-    pivot = array[randint(0, len(array) - 1)]
-
-    for item in array:
-        # Elements that are smaller than the `pivot` go to
-        # the `low` list. Elements that are larger than
-        # `pivot` go to the `high` list. Elements that are
-        # equal to `pivot` go to the `same` list.
-        if item.rating < pivot.rating:
-            low.append(item.rating)
-        elif item.rating == pivot.rating:
-            same.append(item.rating)
-        elif item > pivot:
-            high.append(item.rating)
-
-    # The final result combines the sorted `low` list
-    # with the `same` list and the sorted `high` list
-    return quicksort(low) + same + quicksort(high)
-
-"""
-
-
